99a9e4cc0ea948b651134fe68e0fa006e5fe8566
Morris Horesh <morris_horesh@yahoo.com>
2024-02-22 18:54:29 +0200

    Corrections

--- vpnroute.sh
+++ vpnroute.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 INT_NET=192.168.0.0/24
 UTUN=utun
-for i in $(ifconfig | grep $UTUN | grep flags | awk '{print substr($1, 5)}'| awk '{print substr($0, 1, length($0)-1)}')
-do
-	ifconfig $UTUN$i | grep -- --\>
-	if [ $? -eq 0 ]
-	then
-		TUNNEL=$UTUN$i
-		break
-	fi
-done 
-
-echo $TUNNEL
+EN=en
 
 if [ "$1" = "--reset" ]
 then
 	route -n delete $INT_NET
+	for i in $(ifconfig | grep ^$EN | awk '{print $1}'| awk '{print substr($0, 1, length($0)-1)}')
+	do
+        	ifconfig $i | grep -w inet > /dev/null 2>/dev/null
+        	if [ $? -eq 0 ]
+        	then
+                	ifconfig $i down
+			sleep 2
+			ifconfig $i up
+        	fi
+	done
 else
+	for i in $(ifconfig | grep ^$UTUN | awk '{print $1}'| awk '{print substr($0, 1, length($0)-1)}')
+	do
+        	ifconfig $i | grep -- --\> > /dev/null 2>/dev/null
+        	if [ $? -eq 0 ]
+       		then
+                	TUNNEL=$i
+                	break
+        	fi
+	done
 	sysctl -w net.inet.ip.forwarding=1
 	pfctl -e
 	pfctl -F all
-	echo "nat on $TUNNEL from 192.168.0.0/24 to any -> $TUNNEL" | pfctl -f -
+	CMD="nat on $TUNNEL from $INT_NET to any -> $TUNNEL"
+	echo -------------------- $CMD
+	echo "$CMD" | pfctl -f -
 	ROUTER=$(ifconfig en0 | grep -w inet | awk '{print $2}')
 	route -n delete $INT_NET
 	route -n add $INT_NET $ROUTER
